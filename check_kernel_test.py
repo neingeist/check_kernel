08d16574fc8f0e604cafb4044ec001e16eea2a22
Mike Gerber <mike@sprachgewalt.de>
2015-05-24 19:30:37 +0200

    Support Debian

--- check_kernel_test.py
+++ check_kernel_test.py
@@ -16,6 +16,11 @@ import check_kernel
 
 
 class CleanKernelVersionTestCase(unittest.TestCase):
+    def testDebian(self):
+        versions = [('3.16.7-ckt9-3~deb8u2', '3.16.7-ckt9-3~deb8u2')]
+        for dirty, clean in versions:
+            self.assertEqual(clean_kernel_version(dirty), LooseVersion(clean))
+
     def testFedora(self):
         versions = [('4.0.4-301.fc22.x86_64', '4.0.4-301'),
                     ('4.0.0-1.fc22', '4.0.0-1')]
@@ -28,6 +33,11 @@ class RunningKernelVersionTestCase(unittest.TestCase):
         with unittest.mock.patch.object(check_kernel, 'proc_version', return_value='Linux version 4.0.3-301.fc22.x86_64 (mockbuild@bkernel02.phx2.fedoraproject.org) (gcc version 5.1.1 20150422 (Red Hat 5.1.1-1) (GCC) ) #1 SMP Thu May 21 13:10:33 UTC 2015'):
             self.assertEqual(check_kernel.running_kernel_version(),
                              LooseVersion('4.0.3-301'))
+
+    def testDebian(self):
+        with unittest.mock.patch.object(check_kernel, 'proc_version', return_value='Linux version 3.16.0-4-amd64 (debian-kernel@lists.debian.org) (gcc version 4.8.4 (Debian 4.8.4-1) ) #1 SMP Debian 3.16.7-ckt9-3~deb8u1 (2015-04-24)'):
+            self.assertEqual(check_kernel.running_kernel_version(),
+                             LooseVersion('3.16.7-ckt9-3~deb8u1'))
 
 
 if __name__ == '__main__':
